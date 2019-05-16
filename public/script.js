2ac274133e3203c45a77cb835bcc0e26fc2697a5
Liam Wears <liamwears@fastmail.com>
2019-05-16 14:10:23 +0200

    added comments to script.js
    added comments to script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+//on click of 'demo' button run function result()
 document.getElementById("demo").onclick = function() {result()};
 
 let result = function() {
@@ -16,15 +17,17 @@ let result = function() {
     // change URL depending on where the server is. This had to be changed when testing on mobile devices.
     let urlToFetch = 'http://127.0.0.1:3000/api2'
 
-    //move fetch url to variable
+    //moved fetch url to variable
     fetch(urlToFetch)
-
+        
+        //process the response promise
         .then(function(response) {
             return response.json();
         })
         .then(function (myJson) {
-            //console.log(myJson);
-
+            //console.log(myJson); testing response
+            
+            // if winBig is true build the following element to show the win
             if (myJson.winBig === true) {
                 let element = document.createElement("img");
                 element.setAttribute("id", "whichWin");
@@ -33,6 +36,7 @@ let result = function() {
                 document.getElementById('whichWin').width = "150";
                 document.getElementById("whichWin").height = "150";
                 document.getElementById('whichWin').src = `/images/bigwin.png`;
+            // if winBig is false, smallWin element can be built
             } else if (myJson.winSmall === true) {
                 let element = document.createElement("img");
                 element.setAttribute("id", "whichWin");
@@ -42,7 +46,9 @@ let result = function() {
                 document.getElementById('typeOfWin').innerHTML = "Small WIN!!!";
                 document.getElementById('whichWin').src = `/images/smallwin.png`;
             }
-
+            
+            // same element build but this time for the bonus. 
+            // Also sets 2.5 second pause on the demo button if true to avoid the user clicking again
             if (myJson.winBonus === true) {
                 let element2 = document.createElement("img");
                 element2.setAttribute("id", "whichBonus");
@@ -59,6 +65,14 @@ let result = function() {
 
             }
 
+            /*
+            I had a bit of help with this piece of code,I started off first with a lots of If, Else if statements.
+            Then moved onto a switch statement. then finally onto the below
+            A friend of mine explained how its best to try and slim down the code and not repeat code where unnecessary.
+            First this code runs the forEach on the returned array of 3. Then there are a possible 6 images to choose from,
+            so there is a for loop upto 6 (or 5) to go through the 6 options. Based on what the element in the array is
+            it will choose the corresponding image.
+            */
             myJson.ranNum.forEach((element, index) => {
                 for (let x = 0; x < 6; x++) {
                     if (element === x) {
