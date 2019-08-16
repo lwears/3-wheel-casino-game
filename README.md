52a8bb285f381773f4000fa067980de3d1946ff3
Liam Wears <liamwears@fastmail.com>
2019-08-16 16:39:51 +0200

    updated css and script.js

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # fullstackapp
 code Assignment fullstack app
+
+Below are were the app requirements
+
+Requirements General
+- The application must work in major desktop and mobile devices (please state which devices/
+- browsers the application has been tested on)
+The general principles by which the application will be evaluated are maintainability, testability and
+- reusability.
+Unit Tests are not mandatory but encouraged; the key is that the code is testable.
+
+Client
+- Not external libraries or framework can be used
+- ES6 is not mandatory but encouraged
+- The user must be able to trigger a request for outcome to the server
+- The outcome must be displayed to the user using the provided graphical resources (Symbol_0.png,
+- Symbol_1.png,...)
+- The type of win must be displayed to the user (No Win, Small Win, Big Win)
+A bonus feature must be implemented. If the server returns bonus, the client must first display any win to the user, then trigger an additional request without any user input (this must also be indicated to the user in some form)
+
+Server
+- Server must be a Node.js application
+- Server must be written in ES6
+- Server must be able to serve needed resources to client.
+- Server must be able to receive requests from client and return an outcome (three random integers
+- between 0-5).
+There must be three types of outcomes: No Win, Small Win, Big Win. Two equal integers constitutes a
+- Small Win. Three equal integers constitutes a Big Win.
+Server must randomly (in addition to the outcome) return if bonus feature should be triggered or not.
--- public/script.js
+++ public/script.js
@@ -1,89 +1,80 @@
-//on click of 'demo' button run function result()
-document.getElementById("demo").onclick = function() {result()};
+document.getElementById('demo').onclick = function() {
+	result();
+};
 
 let result = function() {
+	//clear the element to start fresh page
+	if (document.contains(document.getElementById('whichWin'))) {
+		document.getElementById('whichWin').remove();
+	}
+	if (document.contains(document.getElementById('whichBonus'))) {
+		document.getElementById('whichBonus').remove();
+	}
 
-    //clear the element to start fresh page
-    if (document.contains(document.getElementById("whichWin"))) {
-        document.getElementById("whichWin").remove();
-    }
-    if (document.contains(document.getElementById("whichBonus"))) {
-        document.getElementById("whichBonus").remove();
-    }
+	// switch to textContent
+	document.getElementById('typeOfWin').innerHTML = ' ';
+	document.getElementById('typeOfBonus').innerHTML = ' ';
 
-    document.getElementById('typeOfWin').innerHTML = " ";
-    document.getElementById('typeOfBonus').innerHTML = " ";
+	// change URL depending on where the server is. This had to be changed when testing on mobile devices.
+	let urlToFetch = 'http://127.0.0.1:3000/api2';
 
-    // change URL depending on where the server is. This had to be changed when testing on mobile devices.
-    let urlToFetch = 'http://127.0.0.1:3000/api2'
+	//move fetch url to variable
+	fetch(urlToFetch)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(myJson) {
+			//console.log(myJson);
 
-    //moved fetch url to variable
-    fetch(urlToFetch)
-        
-        //process the response promise
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            //console.log(myJson); testing response
-            
-            // if winBig is true build the following element to show the win
-            if (myJson.winBig === true) {
-                let element = document.createElement("img");
-                element.setAttribute("id", "whichWin");
-                document.getElementById('howWin').appendChild(element);
-                document.getElementById('typeOfWin').innerHTML = "BIG WIN!!!";
-                document.getElementById('whichWin').width = "150";
-                document.getElementById("whichWin").height = "150";
-                document.getElementById('whichWin').src = `/images/bigwin.png`;
-            // if winBig is false, smallWin element can be built
-            } else if (myJson.winSmall === true) {
-                let element = document.createElement("img");
-                element.setAttribute("id", "whichWin");
-                document.getElementById('howWin').appendChild(element);
-                document.getElementById('whichWin').width = "150";
-                document.getElementById("whichWin").height = "150";
-                document.getElementById('typeOfWin').innerHTML = "Small WIN!!!";
-                document.getElementById('whichWin').src = `/images/smallwin.png`;
-            }
-            
-            // same element build but this time for the bonus. 
-            // Also sets 2.5 second pause on the demo button if true to avoid the user clicking again
-            if (myJson.winBonus === true) {
-                let element2 = document.createElement("img");
-                element2.setAttribute("id", "whichBonus");
-                document.getElementById('bonusWin').appendChild(element2);
-                document.getElementById('typeOfBonus').innerHTML = "Bonus - Free spin!";
-                document.getElementById('whichBonus').width = "150";
-                document.getElementById("whichBonus").height = "150";
-                document.getElementById('whichBonus').src = `/images/bonus.png`;
-                document.getElementById("demo").disabled = true;
-                setTimeout(function(){document.getElementById("demo").disabled = false;},2500);
-                setTimeout(function() {
-                    result();
-                },2000);
+			if (myJson.winBig === true) {
+				let element = document.createElement('img');
+				element.setAttribute('id', 'whichWin');
+				document.getElementById('howWin').appendChild(element);
+				document.getElementById('typeOfWin').textContent = 'BIG WIN!!!';
+				//document.getElementById('whichWin').width = '150';
+				//document.getElementById('whichWin').height = '150';
+				document.getElementById('whichWin').src = `/images/bigwin.png`;
+			} else if (myJson.winSmall === true) {
+				let element = document.createElement('img');
+				element.setAttribute('id', 'whichWin');
+				document.getElementById('howWin').appendChild(element);
+				//document.getElementById('whichWin').width = "150";
+				//document.getElementById("whichWin").height = "150";
+				document.getElementById('typeOfWin').textContent = 'Small WIN!!!';
+				document.getElementById('whichWin').src = `/images/smallwin.png`;
+			}
 
-            }
+			if (myJson.winBonus === true) {
+				let element2 = document.createElement('img');
+				element2.setAttribute('id', 'whichBonus');
+				document.getElementById('bonusWin').appendChild(element2);
+				document.getElementById('typeOfBonus').textContent = 'Bonus - Free spin!';
+				//document.getElementById('whichBonus').width = "150";
+				//document.getElementById("whichBonus").height = "150";
+				document.getElementById('whichBonus').src = `/images/bonus.png`;
+				document.getElementById('demo').disabled = true;
+				setTimeout(function() {
+					document.getElementById('demo').disabled = false;
+				}, 2500);
+				setTimeout(function() {
+					result();
+				}, 2000);
+			}
 
-            /*
-            I had a bit of help with this piece of code,I started off first with a lots of If, Else if statements.
-            Then moved onto a switch statement. then finally onto the below
-            A friend of mine explained how its best to try and slim down the code and not repeat code where unnecessary.
-            First this code runs the forEach on the returned array of 3. Then there are a possible 6 images to choose from,
-            so there is a for loop upto 6 (or 5) to go through the 6 options. Based on what the element in the array is
-            it will choose the corresponding image.
-            */
-            myJson.ranNum.forEach((element, index) => {
-                for (let x = 0; x < 6; x++) {
-                    if (element === x) {
-                        document.getElementById( "pic" + index ).src = `/images/Symbol_${element}.png`;
-                    }
-                }
-            });
+			myJson.ranNum.forEach((element, index) => {
+				for (let x = 0; x < 6; x++) {
+					if (element === x) {
+						document.getElementById('pic' + index).src = `/images/Symbol_${element}.png`;
+					}
+				}
+			});
+		});
 
-
-        });
-
-
-
+	function isEven(num) {
+		if (num % 2 === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 };
