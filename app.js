07343a0549dac4977c9edf4b8c6614ecb805c99a
lwears <liamwears@fastmail.com>
2020-02-18 21:54:49 +0100

    tidying up frontend script.js still more work to do

--- app.js
+++ app.js
@@ -1,80 +1,55 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
+
 app.use(express.static('public'));
 
-//Create array of 3 numbers no higher than 5
-//https://stackoverflow.com/questions/44164997/can-you-implement-a-while-loop-inside-a-curried-arrow-function-expression
+// Create array of 3 numbers no higher than 5
+// https://stackoverflow.com/questions/44164997/can-you-implement-a-while-loop-inside-a-curried-arrow-function-expression
 const randomNumbers = () => {
-    let arr = [];
-    while (arr.length < 3) {
-        let r = Math.floor(Math.random() * 6);
-        arr.push(r);
-    }
-    return arr;
+  const arr = [];
+  while (arr.length < 3) {
+    const r = Math.floor(Math.random() * 6);
+    arr.push(r);
+  }
+  return arr;
 };
 
-//https://stackoverflow.com/questions/14832603/check-if-all-values-of-array-are-equal
-// const bigWin = arr => arr.every(v => v === arr[0]);
-
-/*
-    //https://www.thepolyglotdeveloper.com/2015/02/calculate-duplicates-exist-array-using-javascript/
-    This function checks if 2 numbers in the array are the same
- */
-// const smallWin = (a) => {
-//     let counts = [];
-//     for(let i = 0; i <= a.length; i++) {
-//         if(counts[a[i]] === undefined) {
-//             counts[a[i]] = 1;
-//         } else {
-//             return true;
-//         }
-//     }
-//     return false;
-// };
-
-//Bonus round, randomly calculate number, if number = 6 bonus triggered
+// Bonus round, randomly calculate number, if number = 6 bonus triggered
 const bonus = () => {
-    let chance = 6;
-    let bonusTrue = Math.floor(Math.random() * chance);
-    return bonusTrue === chance - 1;
+  const chance = 6;
+  const bonusTrue = Math.floor(Math.random() * chance);
+  return bonusTrue === chance - 1;
 };
 
-//Final function, calling all 4 previous functions and returning as one JS object - to be passed to frontend
+// Final function, calling all 4 previous functions and returning as one JS object
+// to be passed to frontend
 const getResult = () => {
-    const ranNum = randomNumbers();
-    const winArr = ranNum.filter((number) => ranNum.indexOf(number) !== ranNum.lastIndexOf(number));
-    const winBig = winArr.length === 3;
-    const winBonus = bonus();
-    let winSmall;
-    if (!winBig) {
-        winSmall = winArr.length === 2;
-    }
+  const ranNum = randomNumbers();
+  const winArr = ranNum.filter((number) => ranNum.indexOf(number) !== ranNum.lastIndexOf(number));
+  const winBig = winArr.length === 3;
+  const winBonus = bonus();
+  let winSmall;
+  if (!winBig) winSmall = winArr.length === 2;
 
-    return { ranNum, winSmall, winBig, winBonus };
+  return {
+    ranNum, winSmall, winBig, winBonus,
+  };
 };
 
-app.get("/index",  (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
-app.get("/api", (req, res) => {
-    res.send(getResult());
+app.get('/api', (req, res) => {
+  res.send(getResult());
 });
 
-app.get('/bonus', (req, res) => {
-    res.json(bonus());
-});
-app.listen(3000, () =>{
-    console.log('Express server started at port 3000');
+app.listen(3000, () => {
+  console.log('Express server started at port 3000');
 });
 
 module.exports.getResult = getResult;
 module.exports.bonus = bonus;
 module.exports.randomNumbers = randomNumbers;
-
-
-
-
-
-
--- public/script.js
+++ public/script.js
@@ -1,70 +1,61 @@
-document.getElementById('demo').onclick = function() {
-	result();
+document.getElementById('demo').onclick = function () {
+  result();
 };
 
-let result = function() {
-	//clear the element to start fresh page
-	if (document.contains(document.getElementById('whichWin'))) {
-		document.getElementById('whichWin').remove();
-	}
-	if (document.contains(document.getElementById('whichBonus'))) {
-		document.getElementById('whichBonus').remove();
-	}
+async function result() {
+  // clear the element to start fresh page
+  if (document.contains(document.getElementById('whichWin'))) {
+    document.getElementById('whichWin').remove();
+  }
+  if (document.contains(document.getElementById('whichBonus'))) {
+    document.getElementById('whichBonus').remove();
+  }
 
-	// switch to textContent
-	document.getElementById('typeOfWin').innerHTML = ' ';
-	document.getElementById('typeOfBonus').innerHTML = ' ';
+  document.getElementById('typeOfWin').textContent = ' ';
+  document.getElementById('typeOfBonus').textContent = ' ';
 
-	// change URL depending on where the server is. This had to be changed when testing on mobile devices.
-	let urlToFetch = 'http://127.0.0.1:3000/api';
+  // change URL depending on where the server is.
+  // This had to be changed when testing on mobile devices.
+  const urlToFetch = 'http://127.0.0.1:3000/api';
 
-	//move fetch url to variable
-	fetch(urlToFetch)
-		.then(response => response.json())
-		.then(function(myJson) {
-			console.log(myJson);
+  function createWinElement(typeOfWin) {
+    const element = document.createElement('img');
+    element.setAttribute('id', 'whichWin');
+    document.getElementById('howWin').appendChild(element);
+    document.getElementById('typeOfWin').textContent = `${typeOfWin.toUpperCase()} Win!!`;
+    document.getElementById('whichWin').src = `/images/${typeOfWin}win.png`;
+  }
 
-			if (myJson.winBig === true) {
-				let element = document.createElement('img');
-				element.setAttribute('id', 'whichWin');
-				document.getElementById('howWin').appendChild(element);
-				document.getElementById('typeOfWin').textContent = 'BIG WIN!!!';
-				//document.getElementById('whichWin').width = '150';
-				//document.getElementById('whichWin').height = '150';
-				document.getElementById('whichWin').src = `/images/bigwin.png`;
-			} else if (myJson.winSmall === true) {
-				let element = document.createElement('img');
-				element.setAttribute('id', 'whichWin');
-				document.getElementById('howWin').appendChild(element);
-				//document.getElementById('whichWin').width = "150";
-				//document.getElementById("whichWin").height = "150";
-				document.getElementById('typeOfWin').textContent = 'Small WIN!!!';
-				document.getElementById('whichWin').src = `/images/smallwin.png`;
-			}
+  function createBonusElement() {
+    const element = document.createElement('img');
+    element.setAttribute('id', 'whichBonus');
+    document.getElementById('bonusWin').appendChild(element);
+    document.getElementById('typeOfBonus').textContent = 'Bonus - Free spin!';
+    document.getElementById('whichBonus').src = '/images/bonus.png';  
+  }  
 
-			if (myJson.winBonus === true) {
-				let element2 = document.createElement('img');
-				element2.setAttribute('id', 'whichBonus');
-				document.getElementById('bonusWin').appendChild(element2);
-				document.getElementById('typeOfBonus').textContent = 'Bonus - Free spin!';
-				//document.getElementById('whichBonus').width = "150";
-				//document.getElementById("whichBonus").height = "150";
-				document.getElementById('whichBonus').src = `/images/bonus.png`;
-				document.getElementById('demo').disabled = true;
-				setTimeout(function() {
-					document.getElementById('demo').disabled = false;
-				}, 2500);
-				setTimeout(function() {
-					result();
-				}, 2000);
-			}
+  const gameResult = await fetch(urlToFetch).then((response) => response.json());
+  console.log(gameResult);
 
-			myJson.ranNum.forEach((element, index) => {
-				for (let x = 0; x < 6; x++) {
-					if (element === x) {
-						document.getElementById('pic' + index).src = `/images/Symbol_${element}.png`;
-					}
-				}
-			});
-		});
-};
+  if (gameResult.winBig) createWinElement('big');
+  if (gameResult.winSmall) createWinElement('small');
+
+  if (gameResult.winBonus) {
+    createBonusElement();
+    document.getElementById('demo').disabled = true;
+    setTimeout(function () {
+      document.getElementById('demo').disabled = false;
+    }, 2500);
+    setTimeout(function () {
+      result();
+    }, 2000);
+  }
+
+  gameResult.ranNum.forEach((element, index) => {
+    for (let x = 0; x < 6; x += 1) {
+      if (element === x) {
+        document.getElementById(`pic${index}`).src = `/images/Symbol_${element}.png`;
+      }
+    }
+  });
+}
